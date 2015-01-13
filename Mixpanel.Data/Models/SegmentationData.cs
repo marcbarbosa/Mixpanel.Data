d3982930708b333d356f53354430677d34e481b3
Marcelo Barbosa <mbarbosa@gmail.com>
2015-01-13 12:33:36 -0200

    New models for segmentation method

---
+++ Mixpanel.Data/Models/SegmentationData.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+
+namespace Mixpanel.Data.Models
+{
+    public class SegmentationData
+    {
+        [JsonProperty(PropertyName = "series")]
+        public List<DateTime> Series { get; set; }
+
+        [JsonProperty(PropertyName = "values")]
+        public Dictionary<string, Dictionary<string, int>> Values { get; set; }
+
+        [JsonProperty(PropertyName = "legend_size")]
+        public int LegendSize { get; set; }
+    }
+}
---
+++ Mixpanel.Data/ResponseModels/SegmentationResponse.cs
@@ -0,0 +1,11 @@
+﻿using Mixpanel.Data.Models;
+using Newtonsoft.Json;
+
+namespace Mixpanel.Data.ResponseModels
+{
+    public class SegmentationResponse
+    {
+        [JsonProperty(PropertyName = "data")]
+        public SegmentationData Data { get; set; }
+    }
+}
